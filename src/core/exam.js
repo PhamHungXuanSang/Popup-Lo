de459b1f92e10c7ca30b2852fa6a6a637f732dde
BeeXuanSang <xuan.sang@beehexa.com>
2023-11-13 20:42:54 +0700

    change exam html

--- src/core/exam.js
+++ src/core/exam.js
@@ -1,18 +1,9 @@
 const htmlPopup = `
 <div class="popup">
-<div class="img-wrapper"><img src="https://png.pngtree.com/thumb_back/fw800/background/20221116/pngtree-isolated-white-background-with-a-vertical-red-gift-bow-photo-image_677817.jpg" alt="Anh"></div>
-<div class="content">
-    <p class="popup-heading">Save up to</p>
-    <h1 class="popup-offer">30% OFF</h1>
-    <p class="popup-desc">In your next holiday offer</p>
-
-    <form action="POST" class="popup-form">
-        <input class="email" type="email" placeholder="Please enter your email">
-        <input class="submit" type="button" value="Get it now">
-    </form>
-
-    <button class="refuse btnClosePopup">No, thanks</button>
-</div>
+    <div style="display: flex;align-items: center; justify-content: center;">
+        <img style="width: 48px; height: 48px; margin: 0 16px;" src="https://ich.edu.vn/App_Files/Upload/2019/icon-thanh-cong.png" alt="icon">
+        <p>Đã thanh toán thành công</p>
+    </div>
 </div>
 `;
 
