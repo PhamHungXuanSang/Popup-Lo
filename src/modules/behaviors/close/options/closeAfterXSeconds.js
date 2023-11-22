8b5e97dea508be8025a8c0d23d33f39d2f609303
tinbeehexa <tin.nguyen@beehexa.com>
2023-11-22 15:23:03 +0700

    22.11.23 update

--- src/modules/behaviors/close/options/closeAfterXSeconds.js
+++ src/modules/behaviors/close/options/closeAfterXSeconds.js
@@ -7,12 +7,10 @@ const closeAfterXSeconds = (config, keyPopup) => {
             mutations.forEach(mutation => {
                 if (mutation.type === "attributes" && mutation.attributeName === "class") {
                     if (elementClosePopup.classList.contains("active")) {
-                        if (!timeoutId) {
-                            timeoutId = setTimeout(() => {
-                                elementClosePopup.classList.remove("active");
-                                timeoutId = null;
-                            }, config.seconds);
-                        }
+                        timeoutId = setTimeout(() => {
+                            elementClosePopup.classList.remove("active");
+                                
+                        }, config.seconds);  
                     } else {
                         clearTimeout(timeoutId);
                     }
